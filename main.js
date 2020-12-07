725cf25abdb844b21b1edecfd684fab94b6f58af
basilalbakkar <74782155+basilalbakkar@users.noreply.github.com>
2020-12-07 05:01:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+var typed = new Typed('.typehome', {
+    strings: ["Web Developer.", "Graphic Designer."],
+    typeSpeed: 100,
+    loop: true
+  });
+
+ 
+
+
+  $(document).ready(function (){
+
+ $(".ann").css("color","rgb(148, 148, 148)");
+ $(".xcircal").css( "display","none");
+ $(".xcircal").css( "font-size","30px");
+  //  nav lift
+ $(".ann").click(function()
+    {
+    $(".ann").css("color","rgb(148, 148, 148)");
+    $(this).css("color","rgb(25, 109, 235");
+   });
+   //  nav top
+  //  $(".navbar-toggler").click(function(){
+  //   $(".sec-menu").show();
+  //   $(".navbar-toggler").css( "display","none");
+  //   $(".xcircal").css( "display","flex");
+  //   // console.log("test hide navbar-toggler ");
+
+  //  }); 
+
+  //  $(".sec-menu").click(function()
+  //    {
+  
+  //   $(".xcircal").css( "display","none");
+  //   $(".navbar-toggler").css( "display","flex");
+  //   // console.log("test hide sec-menu");
+  //  });
+     
+  //  $(".xcircal").click(function(){
+  //   $(".sec-menu").hide();
+  //   $(".navbar-toggler").css( "display","flex");
+  //   $(".xcircal").css( "display","none");
+
+  //   // console.log("test hide sec-menu .xcircal");
+  //  });
+  
+   
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+  
+  });
+  
+  
